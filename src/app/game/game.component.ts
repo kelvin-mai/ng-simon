92616ad2ec76e5d2878ae43cdd182914c324e668
Kelvin Moi <kelvin.mai002@gmail.com>
2018-04-11 21:37:48 -0500

    Completed simon game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Colors } from '../models/colors.enum';
+import { sounds, sleep } from '../models/constants';
+
+const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+};
 
 @Component({
   selector: 'game',
@@ -8,23 +15,27 @@ import { Colors } from '../models/colors.enum';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-  count: number = 1;
+  count: number = 2;
   simon: string[] = [];
   player: string[] = [];
   message: string;
+  messageFail: boolean = true;
+  messageSuccess: boolean = false;
   show: boolean = false;
+  classList = {
+    blue: false,
+    red: false,
+    yellow: false,
+    green: false
+  };
 
-  constructor() {}
+  get randomColor(): string {
+    return Colors[Math.floor(Math.random() * 4)];
+  }
 
   ngOnInit() {
     this.simon = this.generateSimon(this.count);
-    console.log(this.simon);
-  }
-
-  get randomColor(): string {
-    return Colors[
-      Math.floor(Math.random() * (Object.keys(Colors).length / 2 - 1))
-    ];
+    this.showSimon();
   }
 
   generateSimon(num: number): string[] {
@@ -35,51 +46,74 @@ export class GameComponent implements OnInit {
     return arr;
   }
 
-  appendSimon() {
+  appendSimon(): void {
     this.simon.push(this.randomColor);
+    this.showSimon();
   }
 
-  handleClick(e) {
-    if (this.message) this.clearMessage();
-    this.player.push(e);
-    console.log(this.player);
-    this.compareSimon();
+  showSimon() {
+    asyncForEach(this.simon, async item => {
+      this.classList[item] = true;
+      this.playSound(item);
+      await sleep(800);
+      this.classList[item] = false;
+    });
   }
 
-  compareSimon() {
+  compareSimon(): void {
     if (
       this.simon.slice(0, this.player.length).toString() !=
       this.player.toString()
     ) {
+      this.messageFail = true;
+      this.messageSuccess = false;
       this.showMessage('wrong move');
+      sleep(600);
       this.player = [];
+      this.showSimon();
       return;
     }
 
     if (this.simon.length == this.player.length) {
       this.count++;
-      if (this.count == 5) this.showMessage('you win');
+      if (this.count == 20) {
+        this.messageFail = false;
+        this.messageSuccess = true;
+        this.showMessage('you win');
+      }
       this.appendSimon();
       this.player = [];
-
-      console.log(this.simon, this.player);
       return;
     }
   }
 
-  restart() {
-    this.clearMessage();
-    this.count = 1;
-    this.simon = this.generateSimon(this.count);
-    this.player = [];
+  handleClick(e): void {
+    this.playSound(e);
+    if (this.message) this.clearMessage();
+    this.player.push(e);
+    this.compareSimon();
   }
 
-  showMessage(message: string) {
+  restart(): void {
+    this.clearMessage();
+    this.count = 2;
+    this.simon = this.generateSimon(this.count);
+    this.player = [];
+    this.showSimon();
+  }
+
+  showMessage(message: string): void {
     this.message = message;
     this.show = true;
   }
-  clearMessage() {
+
+  clearMessage(): void {
     this.message = '';
     this.show = false;
+  }
+
+  playSound(sound: string) {
+    const audio = new Audio(sounds[sound]);
+    audio.play();
   }
 }
--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,6 +1,8 @@
-export const sounds = {
+export const sounds: any = {
   blue: 'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
   red: 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
   yellow: 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
   green: 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
 };
+
+export const sleep = time => new Promise(resolve => setTimeout(resolve, time));
